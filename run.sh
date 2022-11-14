7c9805d22e204cc4b71eba5c469aaa645acca85d
quyet <code.maito@gmail.com>
2022-11-15 00:58:57 +0700

    temporaty to ignore EC2 frontend for now

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 set -e
 
 envsubst < /etc/nginx/conf.d/api.maitocode.com.conf.tpl > /etc/nginx/conf.d/api.maitocode.com.conf
-envsubst < /etc/nginx/conf.d/www.maitocode.com.conf.tpl > /etc/nginx/conf.d/www.maitocode.com.conf
+# envsubst < /etc/nginx/conf.d/www.maitocode.com.conf.tpl > /etc/nginx/conf.d/www.maitocode.com.conf
 
 nginx -g 'daemon off;'
